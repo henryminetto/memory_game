a771ef2a36dd040c4b17cc65928329a6672be32f
Henry Minetto <98597098+Henry-Minetto@users.noreply.github.com>
2025-05-28 00:05:58 -0300

    monalisa moderna

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,25 +36,34 @@ export default function Home() {
     image: 'https://picsum.photos/200/201',
   }
   ];
-
+  const [shuffledCards, setShuffledCards] = React.useState(items);
+  const [youWin, setYouWin] = React.useState(false);
+  const audioRef = React.useRef<HTMLAudioElement>(null);
   let clicked: number[] = []
+
+  const delay = 1000;
 
   const validatePair = () => {
     const firstId = clicked[0];
     const secondId = clicked[1];
     const firstPair = items.find(item => item.id === firstId)?.sameAs;
+
     if (firstPair == secondId) {
-     //vou deixar pq deve ter alguma animacao/particula
+      clicked = [];
     } else {
       const firstElement = document?.querySelector(`.${styles.card}[data-id="${firstId}"]`);
       const secondElement = document?.querySelector(`.${styles.card}[data-id="${secondId}"]`);
-      console.log('Not a pair', firstElement, secondElement);
+
       if (firstElement && secondElement) {
-        firstElement?.classList.remove(styles.showFace);
-        secondElement?.classList.remove(styles.showFace);
+        setTimeout(() => {
+          firstElement.classList.remove(styles.showFace);
+          secondElement.classList.remove(styles.showFace);
+
+          clicked = [];
+
+        }, delay);
       }
     }
-    clicked = [];
   };
 
   const turnImages = (element: HTMLDivElement) => {
@@ -67,26 +76,39 @@ export default function Home() {
   };
 
   const handleClick = (e: React.MouseEvent<HTMLDivElement>, id: number) => {
+    if (!clicked.includes(id)) {
+      clicked.push(id);
 
-    turnImages(e.currentTarget);
-    clicked.push(id);
-    if (clicked.length === 2) {
-      validatePair();
+      if (clicked.length <= 2) {
+        turnImages(e.currentTarget);
+      }
+      if (clicked.length === 2) {
+        validatePair();
+        return;
+      }
     }
   }
+
+  React.useEffect(() => {
+    setShuffledCards(shuffleArray(items));
+  }, []);
 
   React.useEffect(() => {
     const observer = new MutationObserver(() => {
       const cards = document.querySelectorAll(`.${styles.showFace}`);
       if (cards.length === items.length) {
-        alert('You found all pairs!');
         clicked = [];
-        cards.forEach((card) => {
-          console.log('card', card)
-          const element = card as HTMLDivElement;
-          console.log('element', element)
-          element.classList.remove(styles.showFace);
-        })
+        setYouWin(true);
+        console.time('Game duration');
+        setTimeout(() => {
+          cards.forEach((card) => {
+            const element = card as HTMLDivElement;
+            element.classList.remove(styles.showFace);
+          })
+          setYouWin(false);
+          console.timeEnd('Game duration');
+        }, 300000);
+
       }
     });
 
@@ -98,10 +120,19 @@ export default function Home() {
     return () => observer.disconnect();
   }, []);
 
+  function shuffleArray<T>(array: T[]): T[] {
+    const result = [...array];
+    for (let i = result.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [result[i], result[j]] = [result[j], result[i]];
+    }
+    return result;
+  }
+
   const CardsRender: React.FC = () => {
     return (
       <>
-        {items.map((el: any, i: number) => {
+        {shuffledCards && shuffledCards.map((el: any, i: number) => {
           return (
             <div key={i} className={styles.card} onClick={(e) => handleClick(e, el.id)} data-id={el.id} data-sameas={el.sameAs}>
               <Image src={el.image} alt="" width={200} height={200} className={styles.face}></Image>
@@ -112,12 +143,33 @@ export default function Home() {
       </>
     );
   }
+  // const handlePlayAudio = () => {
+  //   if (audioRef.current) {
+  //     audioRef.current.play(); // Start audio playback
+  //   }
+  // };
+
+  // React.useEffect(() => {
+  //   if (youWin) {
+  //     handlePlayAudio();
+  //   }
+  // }, [youWin]);
 
   return (
     <div className={styles.page}>
       <div className={styles.cardsContainer}>
         <CardsRender></CardsRender>
       </div>
+      {youWin &&
+        <div className={styles.youWin}>
+          <h1>PARABEM</h1>
+          <iframe width="687" height="1221" src="https://www.youtube.com/embed/vx37NHFUqe4?autoplay=1&mute=!" title="DAVI CALMA CALABRESO  #shorts #bbb #bbb24 #reality" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen ></iframe>
+        </div>
+      }
+      {/*   <audio ref={audioRef}>
+       <source src="/calma-calabreso.mp3" type="audio/mpeg" />
+        Your browser does not support the audio element.
+      </audio> */}
     </div>
   );
 }
